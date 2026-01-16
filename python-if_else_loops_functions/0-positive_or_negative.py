194a588275958c0aee54058561460ce07dfac230
Cristian Acevedo <acecristian35@gmail.com>
2026-01-16 10:59:36 -0500

    p0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-number = 98
+number = 98  # or -98 in a different test case
 
 if number > 0:
     print(f"{number} is positive")
@@ -7,4 +7,3 @@ elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
-
