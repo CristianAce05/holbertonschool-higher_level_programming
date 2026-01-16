384dd1d1289d3a5294397603c9fadda69e5af60b
Cristian Acevedo <acecristian35@gmail.com>
2026-01-16 12:52:04 -0500

    p8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,8 @@
 def uppercase(str):
     """Print a string in uppercase followed by a new line"""
     for c in str:
-        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+        print(
+            "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c),
+            end=""
+        )
     print()
