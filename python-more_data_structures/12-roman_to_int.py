153043943ea73f7115c2f9bcf65c52141bfb3d45
Cristian Acevedo <acecristian35@gmail.com>
2026-01-21 13:08:50 -0500

    p12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,18 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
     total = 0
     i = 0
     while i < len(roman_string):
         value = roman_map[roman_string[i]]
-        if i + 1 < len(roman_string) and roman_map[roman_string[i]] < roman_map[roman_string[i+1]]:
-            total += roman_map[roman_string[i+1]] - value
+        if (i + 1 < len(roman_string) and
+                roman_map[roman_string[i]] < roman_map[roman_string[i + 1]]):
+            total += roman_map[roman_string[i + 1]] - value
             i += 2
         else:
             total += value
