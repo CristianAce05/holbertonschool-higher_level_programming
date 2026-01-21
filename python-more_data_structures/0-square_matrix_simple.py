bc33ab41380d782ca55e19e194d511097a3a5c00
Cristian Acevedo <acecristian35@gmail.com>
2026-01-21 12:25:14 -0500

    p0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Returns a new matrix with the square of all integers in the input matrix."""
+    new_matrix = []
+    for row in matrix:
+        new_row = [x ** 2 for x in row]
+        new_matrix.append(new_row)
+    return new_matrix
