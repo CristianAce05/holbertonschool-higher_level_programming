77d9951a8298b80132e5e930751fb3d45b7fa9ba
Cristian Acevedo <acecristian35@gmail.com>
2026-01-21 12:26:28 -0500

    p0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Returns a new matrix with the square of all integers in the input matrix."""
+    """
+    Returns a new matrix with the square of all integers in the input matrix.
+    """
     new_matrix = []
     for row in matrix:
         new_row = [x ** 2 for x in row]
