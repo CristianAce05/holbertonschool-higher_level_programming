b39338916a039c050c24af8f193073ae86302e61
Cristian Acevedo <acecristian35@gmail.com>
2026-01-22 22:00:50 -0500

    p4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
-"""Module that prints a square with the character #."""
+""" Script that prints a square with the character #. """
+
 
 def print_square(size):
-    """Print a square with the character # of given size."""
-    if not isinstance(size, int):
+    """
+    Function that prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If 'size' is not an integer,
+        or if it is a float less than 0.
+        ValueError: If 'size' is less than 0.
+    """
+
+    error = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+
+    if size is None:
+        raise TypeError(error)
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
-    for _ in range(size):
+    if type(size) is float and type(size) < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
         print("#" * size)
