29bd96edeae0540672c1391422461fdc84529222
Cristian Acevedo <acecristian35@gmail.com>
2026-01-22 21:47:14 -0500

    p0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Module that adds two integers.
-
-This module defines a function to add two numbers.
-Numbers can be integers or floats.
-Floats are cast to integers before addition.
-"""
+"""Script that adds two integers"""
 
 
 def add_integer(a, b=98):
-    """Add two integers.
-
-    a and b must be integers or floats.
-    Returns the integer sum of a and b.
     """
-    if not isinstance(a, (int, float)):
+    Function that adds two integers.
+    Args:
+        a: The first number, must be an integer or float.
+        b: The second number, must be an integer or float, defaults to 98.
+    Returns:
+        The sum of the two numbers as an integer.
+    Raises:
+        TypeError: If a or b is not an integer or float, or if they are None.
+    """
+    error = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if a is None or b is None:
+        raise TypeError(error)
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
