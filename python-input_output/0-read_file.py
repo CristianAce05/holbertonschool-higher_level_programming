dec14854bfb65a39a08dc1c0f9924b02bb00b599
Cristian Acevedo <acecristian35@gmail.com>
2026-02-09 22:01:38 -0500

    p0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Reads a text file (UTF-8) and prints it to stdout."""
+
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
+
