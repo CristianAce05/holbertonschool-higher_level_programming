13b345a708c1e8f87ec1c647aecadce1e0489e63
Cristian Acevedo <acecristian35@gmail.com>
2026-02-09 22:17:14 -0500

    p2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Appends a string to a UTF-8 text file and returns the number of characters added."""
+"""Append text to a UTF-8 file and return the number of characters added."""
 
 
 def append_write(filename="", text=""):
-    """Append a string to a text file and return the number of characters written."""
+    """Append text to a file and return the number of characters written."""
     with open(filename, mode="a", encoding="utf-8") as f:
         return f.write(text)
