4d4c9838ce655b309f5b1fbb9023104e943c2ab9
Cristian Acevedo <acecristian35@gmail.com>
2026-02-09 22:29:58 -0500

    p8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Return the dictionary description of a class instance for JSON serialization."""
+"""Return the dictionary description of a class instance for JSON use."""
 
 
 def class_to_json(obj):
