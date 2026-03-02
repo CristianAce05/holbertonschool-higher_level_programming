16ea6f86a30b0ebe51fefcb550cf4342ef20b68d
Cristian Acevedo <acecristian35@gmail.com>
2026-03-02 10:37:24 -0500

    p5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Print the description of first_table without using DESCRIBE or EXPLAIN
-SELECT column_name AS Field, column_type AS Type, is_nullable AS Null, column_key AS Key
-FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table';
+-- Display the full CREATE TABLE statement of first_table
+SHOW CREATE TABLE first_table;
