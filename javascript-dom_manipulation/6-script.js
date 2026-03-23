e65f69e1f74a524023dccc099e8af2b89070a79f
Cristian Acevedo <acecristian35@gmail.com>
2026-03-23 11:00:39 -0400

    p6

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,7 +1,16 @@
 const character = document.querySelector('#character');
 
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch character');
+    }
+
+    return response.json();
+  })
   .then((data) => {
     character.textContent = data.name;
+  })
+  .catch(() => {
+    character.textContent = 'Unable to load character';
   });
