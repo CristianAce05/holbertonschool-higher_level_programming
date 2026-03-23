3d001a4ed35df4bcfbe3f8b93f2de70bd088fd6b
Cristian Acevedo <acecristian35@gmail.com>
2026-03-23 11:00:48 -0400

    p7

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,7 +1,13 @@
 const listMovies = document.querySelector('#list_movies');
 
 fetch('https://swapi-api.hbtn.io/api/films/?format=json')
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to fetch movies');
+    }
+
+    return response.json();
+  })
   .then((data) => {
     data.results.forEach((movie) => {
       const listItem = document.createElement('li');
@@ -9,4 +15,7 @@ fetch('https://swapi-api.hbtn.io/api/films/?format=json')
       listItem.textContent = movie.title;
       listMovies.appendChild(listItem);
     });
+  })
+  .catch(() => {
+    listMovies.textContent = 'Unable to load movies';
   });
